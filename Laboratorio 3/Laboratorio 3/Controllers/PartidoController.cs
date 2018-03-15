694bf2714b999e31f4b59b2fb91c31065c28e464
josue0ghost <llaajjee@hotmail.com>
2018-03-14 23:22:59 -0600

    Create, Delete and Details views w/ Log

--- Laboratorio 3/Laboratorio 3/Controllers/PartidoController.cs
+++ Laboratorio 3/Laboratorio 3/Controllers/PartidoController.cs
@@ -18,21 +18,35 @@ namespace Laboratorio_3.Controllers
             return View();
         }
 
-        // GET: Partido/Details/5
-        public ActionResult Details(int id)
+        // GET: Partido
+        public ActionResult IndexPartidoFecha()
         {
             return View();
         }
 
+        // GET: Partido/Details/5
+        public ActionResult DetailsPartido(int id)
+        {
+            var partido = Data.Instance.listaPartidos.Where(x => x.noPartido == id).FirstOrDefault();
+            return View(partido);
+        }
+
+        // GET: Partido/Details/5
+        public ActionResult DetailsPartidoFecha(int id)
+        {
+            var partido = Data.Instance.listaPartidos.Where(x => x.fechaPartido == Convert.ToDateTime(id)).FirstOrDefault();
+            return View(partido);
+        }
+
         // GET: Partido/Create
-        public ActionResult Create()
+        public ActionResult CreateFechaPartido()
         {
             return View();
         }
 
         // POST: Partido/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult CreateFechaPartido(FormCollection collection)
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
@@ -49,7 +63,11 @@ namespace Laboratorio_3.Controllers
                 });
                 Data.Instance.listaPartidos = Data.Instance.partidosAVL.Orders("InOrder");
 
-                return RedirectToAction("Index");
+                stopwatch.Stop();
+                TimeSpan ts = stopwatch.Elapsed;
+                Log.SendToLog("Creating a new item 'Partido' to Data.Instance.partidosAVL from Create view", ts);
+
+                return RedirectToAction("IndexPartidoFecha");
             }
             catch
             {
@@ -57,21 +75,36 @@ namespace Laboratorio_3.Controllers
             }
         }
 
-        // GET: Partido/Edit/5
-        public ActionResult Edit(int id)
+        // GET: Partido/Create
+        public ActionResult CreateNoPartido()
         {
             return View();
         }
 
-        // POST: Partido/Edit/5
+        // POST: Partido/Create
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult CreateNoPartido(FormCollection collection)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             try
             {
-                // TODO: Add update logic here
+                Data.Instance.partidosAVL.Insert(new Partido
+                {
+                    noPartido = Convert.ToInt16(collection["Número de partido"]),
+                    fechaPartido = Convert.ToDateTime(collection["Fecha de partido"]),
+                    Grupo = collection["Grupo"],
+                    pais1 = collection["Pais No. 1"],
+                    pais2 = collection["Pais No. 2"],
+                    estadio = collection["Estadio"]
+                });
+                Data.Instance.listaPartidos = Data.Instance.partidosAVL.Orders("InOrder");
 
-                return RedirectToAction("Index");
+                stopwatch.Stop();
+                TimeSpan ts = stopwatch.Elapsed;
+                Log.SendToLog("Creating a new item 'Partido' to Data.Instance.partidosAVL from Create view", ts);
+
+                return RedirectToAction("IndexPartido");
             }
             catch
             {
@@ -80,7 +113,39 @@ namespace Laboratorio_3.Controllers
         }
 
         // GET: Partido/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult DeleteFechaPartido(int id)
+        {
+            var partido = Data.Instance.listaPartidos.Find(x => x.fechaPartido == Convert.ToDateTime(id));
+            return View(partido);
+        }
+
+        // POST: Partido/Delete/5
+        [HttpPost]
+        public ActionResult DeleteFechaPartido(int id, FormCollection collection)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            try
+            {
+                // TODO: Add delete logic here
+                Partido partido = Data.Instance.listaPartidos.Find(x => x.fechaPartido == Convert.ToDateTime(id));
+                Data.Instance.partidosAVL.Eliminar(partido);
+                Data.Instance.listaPartidos = Data.Instance.partidosAVL.Orders("InOrder");
+
+                stopwatch.Stop();
+                TimeSpan ts = stopwatch.Elapsed;
+                Log.SendToLog("Deleting a new item 'Partido' to Data.Instance.partidosAVL from Delete view", ts);
+
+                return RedirectToAction("IndexPartidoFecha");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Partido/Delete/5
+        public ActionResult DeletePartido(int id)
         {
             var partido = Data.Instance.listaPartidos.Find(x => x.noPartido == id);
             return View(partido);
@@ -88,15 +153,22 @@ namespace Laboratorio_3.Controllers
 
         // POST: Partido/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult DeletePartido(int id, FormCollection collection)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             try
             {
                 // TODO: Add delete logic here
                 Partido partido = Data.Instance.listaPartidos.Find(x => x.noPartido == id);
                 Data.Instance.partidosAVL.Eliminar(partido);
                 Data.Instance.listaPartidos = Data.Instance.partidosAVL.Orders("InOrder");
-                return RedirectToAction("Index");
+
+                stopwatch.Stop();
+                TimeSpan ts = stopwatch.Elapsed;
+                Log.SendToLog("Deleting a new item 'Partido' to Data.Instance.partidosAVL from Delete view", ts);
+
+                return RedirectToAction("IndexPartido");
             }
             catch
             {
